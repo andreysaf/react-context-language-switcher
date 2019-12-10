b0672cb56219030b018cfa04d51f595947fa6c6f
Andrey Safonov <a.so777@live.ca>
2019-12-09 22:20:31 -0800

    Added consumer way of extracting the data.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,21 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
-    static contextType = LanguageContext;
+    // static contextType = LanguageContext;
 
     render () {
-        const text = this.context === 'english' ? 'Submit' : 'Применить';
-        return <button className="ui button primary">{text}</button>;
+        // const text = this.context === 'english' ? 'Submit' : 'Применить';
+        // return <button className="ui button primary">{text}</button>;
+
+        return (
+            <button className="ui button primary">
+                <LanguageContext.Consumer>
+                    {(value) => value === 'english' ? 'Submit' : 'Применить'}
+                </LanguageContext.Consumer>
+            </button>
+        );
     }
 }
+
 
 export default Button;
